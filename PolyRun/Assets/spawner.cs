f6db574e9f48533f86ced81970a3f603b88cf691
codeninjascf <cnchandlersford@gmail.com>
2023-02-12 11:03:47 +0000

    dsz\
     x x   hsdedgbsfgysdsdhufsyh8un,  gn nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn

--- PolyRun/Assets/spawner.cs
+++ PolyRun/Assets/spawner.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public float objectSpeed = 8f;
-    private List<GameObject>_activeObject;
+    private List<GameObject>_activeObjects;
 
     void start()
     {
-     _activeObject = new List<GameObject>();
+     _activeObjects = new List<GameObject>();
      StartCoroutine(Spawn());
     }
     
     void Update()
-    {
+    { 
         Vector3 movement = objectSpeed * Time.deltaTime * Vector3.left;
-        foreach (GameObject activeObject in _activeObject)
+        foreach (GameObject activeObject in _activeObjects)
         {
             activeObject.transform.position += movement;
         }
@@ -25,10 +25,10 @@ public class Spawner : MonoBehaviour
 
     IEnumerator Spawn()
     {
-        GameManager.UpdateList(_activeObject);
+        GameManager.UpdateList(_activeObjects);
         GameObject challengeObject = Instantiate(GameManager.GetChallengeObject());
         challengeObject.transform.position = new Vector3(GameManager.ScreenBounds.x, 0);
-        _activeObject.Add(challengeObject);
+        _activeObjects.Add(challengeObject);
         ChallengeObject script = challengeObject.GetComponent<ChallengeObject>();
         yield return new WaitForSeconds(script.challengeTime);
         StartCoroutine(Spawn());
