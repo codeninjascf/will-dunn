780410a63d9769bc4722e968e3ca1074973514a3
codeninjascf <cnchandlersford@gmail.com>
2023-04-30 11:38:14 +0100

    another day, another slay
    e

--- robomaniaa/Assets/Scripts/EnemyMovement.cs
+++ robomaniaa/Assets/Scripts/EnemyMovement.cs
@@ -26,11 +26,12 @@ public class EnemyMovement : MonoBehaviour
             {
                 return -1;
             }
-
-        }   
-
-            
-
+            if (GameManager.Player.transform.position.x > transform.position.x)
+            {
+                return 1;
+            }
+            return 0;
+        }
         set
         {
 
@@ -38,9 +39,23 @@ public class EnemyMovement : MonoBehaviour
     }
     void Start()
     {
-        
+        _rigidbody.velocity = Direction * moveSpeed * Vector2.right; 
     }
 
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            _rigidbody.velocity = Direction * moveSpeed * Vector2.right;
+            _rigidbody.AddForce(jumpForce * Vector2.up, ForceMode2D.Impulse);
+        }
+        else if (other.gameObject.CompareTag("Player"))
+        {
+            _rigidbody.velocity = Direction * moveSpeed * Vector2.left;
+            _rigidbody.AddForce(jumpForce * Vector2.up, ForceMode2D.Impulse);
+            _rigidbody.AddForce(jumpForce * Vector2.up, ForceMode2D.Impulse);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
