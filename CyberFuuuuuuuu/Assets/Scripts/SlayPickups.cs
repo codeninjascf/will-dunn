a9d0ba7e25b3feaa8424923f7c432e8bfe9e048f
codeninjascf <105656685+codeninjascf@users.noreply.github.com>
2023-05-14 11:30:24 +0100

    slay day yay

---
+++ CyberFuuuuuuuu/Assets/Scripts/SlayPickups.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlayPickups : MonoBehaviour
+{
+    public int healAmount = 2;
+    public float rotateSpeed = 50f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+      if (other.CompareTag("Player"))
+        {
+            other.GetComponent<PlayerHealth>().Add(healAmount);
+
+            Destroy(gameObject);
+        }
+    }
+}
