0b4d60fb3d42cccf583c8e9295471f7e1d2d4669
codeninjascf <cnchandlersford@gmail.com>
2024-01-14 11:55:05 +0000

    hfddhhdhedjjdjjjfdjjjjjdjjdjdjdjdjjjdddddddddjjjddddddjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjdghdgggduudiidihdhdjgiijgijjgigjgughgjghgjghg

--- food frenze/Assets/Scripts/GameManager.cs
+++ food frenze/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,19 +9,59 @@ public class GameManager : MonoBehaviour
     public GridManager gridManager;
 
     private bool _gameEnded;
+
+    public UIManager uiManager;
     void Start()
     {
-       _gameEnded = false;   
+       _gameEnded = false;
+        
+        if(level.type == LevelType.Moves)
+        {
+            uiManager.criteriaInfoText.text = "MovesRemaining:";
+        }
+        else if(level.type == LevelType.Time)
+        {
+            uiManager.criteriaInfoText.text = "TimeRemaining";
+        }
+        uiManager.highScoreNumber.text = level.HighScore.ToString();
+        uiManager.gameOverMenu.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-     if(level.GameOver && gridManager.MoveComplete && !_gameEnded)  
+        if(level.type == LevelType.Moves)
+        {
+            uiManager.criteriaNumber.text = level.MovesRemaining.ToString();
+        }
+        else if(level.type == LevelType.Time)
+        {
+            double timeRemaining = Math.Max(level.TimeRemaining, 0);
+            TimeSpan time = TimeSpan.FromSeconds(timeRemaining);
+            uiManager.criteriaNumber.text = time.ToString(@"mm/:ss");
+        }
+
+        int starsAchieved = level.StarsAchieved(gridManager.Score);
+
+        uiManager.UpdateScores(gridManager.Score);
+        uiManager.UpdateStars(starsAchieved);
+
+        if(level.GameOver && gridManager.MoveComplete && !_gameEnded)  
      {
         _gameEnded = true;
 
         gridManager.GameActive = false;
+
+            string gameOverText = starsAchieved switch
+            {
+                 0 => "Try Again!",
+                 1 => "Good Job!",
+                 2 => "Amazing!",
+                 3 => "YOU ARE THE FOOD MASTER!!!!"
+            };
+            uiManager.gameOverText.text = gameOverText;
+            uiManager.newHighScoreText.SetActive(gridManager.Score > level.HighScore);
+            uiManager.gameOverMenu.SetActive(true);
 
         level.UpdateHighScore(gridManager.Score);
         level.UpdateStarsAchieved(gridManager.Score);
