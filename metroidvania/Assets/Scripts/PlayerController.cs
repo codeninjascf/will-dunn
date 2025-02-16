f7c8dd4da3317ae0133acb5e15247102b0a9df1b
codeninjascf <cnchandlersford@gmail.com>
2025-02-16 11:14:38 +0000

    34

--- metroidvania/Assets/Scripts/PlayerController.cs
+++ metroidvania/Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb;
 
