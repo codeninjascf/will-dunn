1b078c11931a3b332031d9d59584d21a5ce9a967
codeninjascf <105656685+codeninjascf@users.noreply.github.com>
2025-03-09 10:47:57 +0000

    eee

--- metroidvania/Assets/Scripts/PlayerController.cs
+++ metroidvania/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
 
     private bool canDoubleJump;
 
+    
     public Rigidbody2D rb;
 
     public Transform groundPoint;
@@ -20,10 +21,60 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 20;
 
     public Animator anim;
+    
+    public float dashSpeed, dashTime;
+    private float dashCounter;
+
+    public SpriteRenderer theSR, afterImage;
+    public float afterImageLifetime, timeBetweenAfterImages;
+    private float afterImageCounter;
+    public Color afterImageColor;
+
+    public float waitAfterDashing;
+    private float dashRechargeCounter;
 
     public void Update()
     {
-        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, rb.velocity.y);
+        if(dashRechargeCounter > 0)
+        {
+            dashRechargeCounter -= Time.deltaTime;
+        }
+
+        if(Input.GetButtonDown("Fire2"))
+        {
+            dashCounter = dashTime;
+
+            ShowAfterImage();
+        }
+           dashRechargeCounter = waitAfterDashing;
+
+        if(dashCounter > 0)
+        {
+            dashCounter = dashCounter - Time.deltaTime;
+
+            rb.velocity = new Vector2(dashSpeed * transform.localScale.x, rb.velocity.y);
+
+            afterImageCounter -= Time.deltaTime;
+            if(afterImageCounter <= 0)
+            {
+                ShowAfterImage();
+            }
+        }
+        else
+        {
+            rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, rb.velocity.y);
+
+            if (rb.velocity.x < 0)
+            {
+                transform.localScale = new Vector3(-1f, 1f, 1f);
+            }
+            else if (rb.velocity.x > 0)
+            {
+                transform.localScale = Vector3.one;
+            }
+        }
+
+        
 
         if (Input.GetButtonDown("Jump") && (onGround || canDoubleJump))
         {
@@ -39,14 +90,7 @@ public class PlayerController : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
 
         }
-        if (rb.velocity.x < 0)
-        {
-            transform.localScale = new Vector3(-1f, 1f, 1f);
-        }
-        else if (rb.velocity.x > 0)
-        {
-            transform.localScale = Vector3.one;
-        }
+        
 
         onGround = Physics2D.OverlapCircle(groundPoint.position, .2f, whatIsGround);
 
@@ -60,5 +104,17 @@ public class PlayerController : MonoBehaviour
         }
 
 
+    }
+
+    public void ShowAfterImage()
+    {
+        SpriteRenderer image = Instantiate(afterImage, transform.position, transform.rotation);
+        image.sprite = theSR.sprite;
+        image.transform.localScale = transform.localScale;
+        image.color = afterImage.color;
+
+        Destroy(image.gameObject,afterImageLifetime);
+
+        afterImageCounter = timeBetweenAfterImages;
     }
 }
