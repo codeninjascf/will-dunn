0afb07489f74a3aaf1bd5298205fe6b87d13a814
codeninjascf <cnchandlersford@gmail.com>
2025-04-06 11:17:36 +0100

    tt

---
+++ metroidvania/Assets/Scripts/Ability Unlock.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityUnlock : MonoBehaviour
+{
+    public bool unlockDoubleJump, unlockDash, unlockBall, unlockDropBomb;
+
+    public GameObject AbilityEffect;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            PlayerAbilityTracker player = other.GetComponentInParent<PlayerAbilityTracker>();
+
+            if (unlockDoubleJump)
+            {
+                player.canDoubleJump = true;
+            }
+            if (unlockDash)
+            {
+                player.canDash = true;
+            }
+
+            if (unlockDoubleJump)
+            {
+                player.canBecomeBall = true;
+            }
+            if (unlockDropBomb)
+            {
+                player.canDropBomb = true;
+            }
+
+
+            Destroy(gameObject);
+        }
+
+        
+    }
+}
