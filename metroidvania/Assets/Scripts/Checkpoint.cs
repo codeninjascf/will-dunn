bb0522f8a17a1488d4ad1e1506a594a116fdf6f6
codeninjascf <cnchandlersford@gmail.com>
2025-06-15 11:22:09 +0100

    e

--- metroidvania/Assets/Scripts/Checkpoint.cs
+++ metroidvania/Assets/Scripts/Checkpoint.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(other.tag == "Player")
+        {
+            RespawnController.instance.SetSpawn(transform.position);
+        }   
     }
 }
